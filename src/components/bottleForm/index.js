2199ddb0630584de52375f55c96a944f99d89816
Christine Yueh <christinecyueh@gmail.com>
2019-08-23 12:06:46 -0400

    added ability to update bottle details

--- src/components/bottleForm/index.js
+++ src/components/bottleForm/index.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-function BottleForm(props) {
-  return (
-    <form onSubmit={props.addBottle}>
-      <div className="form-group">
-        <label>Producer</label>
-        <input type="text" className="form-control" name="producer" />
-        <label>Name/Varietal</label>
-        <input type="text" className="form-control" name="bottle_name" />
-        <label>Vintage</label>
-        <input type="number" className="form-control" name="vintage" />
-        <label>Label Image</label>
-        <input type="text" className="form-control" name="label_img" />
-      </div>
-      <button type="submit" className="btn btn-primary">Add Bottle</button>
-    </form>
-  );
+class BottleForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      producer: '',
+      bottle_name: '',
+      vintage: '',
+      label_img: ''
+    }
+  }
+
+  async componentDidMount() {
+    let bottle = await this.props.bottle;
+
+    if (bottle.bottle_id) {
+      this.setState({
+        'producer': bottle.producer,
+        'bottle_name': bottle.bottle_name,
+        'vintage': bottle.vintage,
+        'label_img': bottle.label_img
+      });
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.addBottle}>
+        <div className="form-group">
+          <label>Producer</label>
+          <input type="text" className="form-control" name="producer" defaultValue={this.state.producer}/>
+          <label>Name/Varietal</label>
+          <input type="text" className="form-control" name="bottle_name" defaultValue={this.state.bottle_name} />
+          <label>Vintage</label>
+          <input type="number" className="form-control" name="vintage" defaultValue={this.state.vintage} />
+          <label>Label Image</label>
+          <input type="text" className="form-control" name="label_img" defaultValue={this.state.label_img} />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          {this.state.label_img ? "Edit" : "Add" } Bottle
+        </button>
+      </form>
+    );
+  }
 }
 
 export default BottleForm;
--- src/views/bottle/add/index.js
+++ src/views/bottle/add/index.js
@@ -5,16 +5,37 @@ import Form from '../../../components/form';
 import BottleForm from '../../../components/bottleForm';
 
 class AddBottle extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      token: '',
+      user_id: '',
+      party_id: '',
+      bottle: {}
+    }
+  }
+
+  getDetails = async(e) => {
+    let token = this.props.token;
+    let user_id = JSON.parse(atob(token.split('.')[1])).user_id;
+    let party_id = this.props.history.location.state.party_id;
+
+    return [token, user_id, party_id];
+  }
+
   addBottle = async(e) => {
     e.preventDefault();
 
-    let token = this.props.token;
-    let user_id = JSON.parse(atob(token.split('.')[1])).user_id;
     let data_json = {};
 
     Object.values(e.target.elements).map(k => { if (k.name.length > 0) data_json[k.name] =  k.value } );
-    data_json['user_id'] = user_id;
-    data_json['party_id'] = this.props.history.location.state.party_id;
+    data_json['user_id'] = this.state.user_id;
+    data_json['party_id'] = this.state.party_id;
+    
+    if (this.state.bottle.bottle_id) {
+      data_json['bottle_id'] = this.state.bottle.bottle_id;
+    }
 
     const URL = 'http://localhost:5000/api/bottles/save';
 
@@ -35,11 +56,42 @@ class AddBottle extends Component {
     }
   }
 
+  retrieveBottle = async(user_id, party_id) => {
+    let URL = 'http://localhost:5000/api/bottles/retrieve?party_id=';
+    URL += party_id;
+    URL += '&user_id=' + user_id;
+
+    let response = await fetch(URL, {
+      'method': 'GET',
+      'headers': { 'Content-Type': 'application/json' }
+    })
+
+    let data = await response.json();
+    if (data.success) {
+      return data.bottles;
+    } else if (data.error) {
+      alert(`${data.error}`);
+    } else {
+      alert('Sorry, try again.');
+    }
+  }
+
+  async componentDidMount() {
+    let details = await this.getDetails();
+    let bottle = await this.retrieveBottle(details[1], details[2]);
+    this.setState({
+      'token': details[0],
+      'user_id': details[1],
+      'party_id': details[2],
+      'bottle': bottle
+    });
+  }
+
   render() {
-    if (this.props.token) {
+    if (this.state.token) {
       return (
-        <Form title="Add Bottle Details">
-          <BottleForm addBottle={this.addBottle} />
+        <Form title="">
+          <BottleForm addBottle={this.addBottle} bottle={this.state.bottle} />
         </Form>
       );
     } else {
